2928583030f7e92e28e5464e430a1a6153843e84
Andres Chaverra <adochapi2000@gmail.com>
2023-07-28 20:50:27 -0500

    first

---
+++ src/Components/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Logo from "../Assets/Logo.svg";
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { BsYoutube } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section-one">
+        <div className="footer-logo-container">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="footer-icons">
+          <BsTwitter />
+          <SiLinkedin />
+          <BsYoutube />
+          <FaFacebookF />
+        </div>
+      </div>
+      <div className="footer-section-two">
+        <div className="footer-section-columns">
+          <span>Calidad</span>
+          <span>Ayuda</span>
+          <span>Trabaje con nosotros</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>+57 313 456 78 90</span>
+          <span>hello@food.com</span>
+          <span>press@food.com</span>
+          <span>contact@food.com</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>Terminos y COndiciones</span>
+          <span>Politica de Privacidad</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
